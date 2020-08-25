403cca2a09420bafc5612f348f5156115e01c3f2
Mattiassonjesper <57625574+Mattiassonjesper@users.noreply.github.com>
2020-08-25 09:50:56 +0200

    Lesson1
    Connect to peer server

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+const peerOnOpen = (id) => {
+    document.querySelector(".my-peer-id").innerHTML = id;
+};
+
+const myPeerId = location.hash.slice(1);
+console.log(myPeerId)
+
+let peer = new Peer(myPeerId, {
+    host: "glajan.com",
+    port: 8443,
+    path: "/myapp",
+    secure: true, 
+    });
+
+peer.on("open", peerOnOpen);
+
 
